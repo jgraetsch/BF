1a7df8e4b487d0b65e6f121cb34f6c35c816d70d
jgraetsch <graetsch@msn.com>
2016-04-07 17:22:52 +0200

    1. Test

--- BF/BF/Program.cs
+++ BF/BF/Program.cs
@@ -11,7 +11,7 @@ namespace BF
         // wahrscheinlichen Zeichensatz definieren
         static char[] Match =
         {
-                '0','1','2','3','4'
+                '0','1','2','3','4','5','6','7','8','9'
             };
 
         // unser Passwort
@@ -21,9 +21,47 @@ namespace BF
 
         static void Main(string[] args)
         {
-           
+            space = " ";
+            int Count;
+            Console.WriteLine("Willkommen bei unserem Brute Force Test.");
+            Console.WriteLine(space);
+            Console.WriteLine("Geben Sie das Test-Passwort ein: ");
 
+            // Init Passwort
+            FindPassword = (Console.ReadLine());
 
+            // Uhrzeit setzen
+            DateTime today = DateTime.Now;
+            today.ToString("dd.mm.yyyy HH:mm:ss");
+            Console.WriteLine(space);
+            Console.WriteLine("START:\t{0}",today);
+
+            for (Count=0;Count<=15;Count++)
+            {
+                Recurse(Count, 0, "");
+            }
+        }
+
+        static void Recurse (int Length, int Position, string BaseString)
+        {
+            int Count = 0;
+            for (Count = 0; Count < Match.Length; Count++)
+            {
+                Combi++;
+                if (Position<Length -1)
+                {
+                    Recurse(Length, Position + 1, BaseString + Match[Count]);
+                }
+                if (BaseString + Match[Count]==FindPassword)
+                {
+                    DateTime today = DateTime.Now;
+                    today.ToString("dd.mm.yyyy HH:mm:ss");
+                    Console.WriteLine(space);
+                    Console.WriteLine("END:\t{0}\nCombi:\t{1}",today,Combi);
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+            }
         }
     }
 }
