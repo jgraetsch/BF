72db4ac2362b4113fe685fa4cfb179a2b7b3b525
jgraetsch <graetsch@msn.com>
2016-04-07 17:03:25 +0200

    Init

---
+++ BF/BF/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BF
+{
+    class Program
+    {
+        // wahrscheinlichen Zeichensatz definieren
+        static char[] Match =
+        {
+                '0','1','2','3','4'
+            };
+
+        // unser Passwort
+        static string FindPassword;
+        static int Combi;
+        static string space;
+
+        static void Main(string[] args)
+        {
+           
+
+
+        }
+    }
+}
